fd1291c91fc7647e8f34a06c0c7cc2f66f77f6e9
arturo-19 <arturo_jtr@hotmail.com>
2018-10-20 01:40:12 -0600

    Tarea 1 "Hola Mundo"

---
+++ Hola mundo/Win.Hola mundo/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Win.Hola_mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(136, 30);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(106, 49);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "SALUDAR";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.Maroon;
+            this.txt1.Location = new System.Drawing.Point(98, 160);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(176, 29);
+            this.txt1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(385, 261);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.bt1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hola Mundo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
